927d72109aa315011a501befb4a190cd6d5b75f7
Oğuzhan Tekin <119113636+tekinoguzhan@users.noreply.github.com>
2023-09-07 02:53:53 +0300

    Add files via upload

---
+++ src/main/java/com/otekin/dev/SpringbootInterceptorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.otekin.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterceptorDemoApplication.class, args);
+	}
+
+}
